85283a59478b8e56fe4a12ccbfb98615d53eeb87
Yashkumar Gupta <yash.gupta02@yahoo.in>
2022-12-09 23:33:41 +0530

    backend

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+
+const port = process.env.PORT || 5000
+
+const app = express()
+
+//Enable body parser
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/openai', require('./routes/openaiRoutes'))
+
+app.listen(port, () => console.log(`Server started at port ${port}...`))
