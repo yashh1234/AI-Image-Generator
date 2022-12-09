cfdea73489930cd8d9ca49475ae1f690cec36631
Yashkumar Gupta <yash.gupta02@yahoo.in>
2022-12-10 00:12:46 +0530

    added files

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv').config()
 
@@ -8,6 +9,8 @@ const app = express()
 //Enable body parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/openai', require('./routes/openaiRoutes'))
 
